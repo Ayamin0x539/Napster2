bb65e2ac44086155bd9437e0dfc12fd12558cd22
Jonathan Roemer <jon@roemersoftworks.com>
2015-12-05 23:02:15 -0500

    A little add track work, committing so I can push and then pull

--- napster2/napster2/forms.py
+++ napster2/napster2/forms.py
@@ -60,3 +60,9 @@ class OrderForm(forms.Form):
 
 class AddTrack(forms.Form):
     trackname = forms.CharField(label='Track Name', max_length=40, required=True)
+    albumname = forms.CharField(label='Album', max_length=40, required=True)
+    mediatype = forms.CharField(label='Media Type', max_length=40, required=True)
+    composer  = forms.CharField(label='Composer', max_length=40, required=True)
+    length    = forms.IntegerField(label='Length in Milliseconds', max_length=40, required=True)
+    size      = forms.IntegerField(label='Size in bytes', max_length=40, required=True)
+    
